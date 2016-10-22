6a849986ba38a272c7c1ab8bac5fd77bb1d3d7af
Mario Finelli <mario@finel.li>
2016-10-21 22:04:18 -0400

    Add pugjs and initial index view

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('index');
 });
 
 app.listen(3000, function() {
