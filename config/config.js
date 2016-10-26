dd477bcac41e5d3d9120b60e7978854f2d283f03
Mario Finelli <mario@finel.li>
2016-10-26 08:52:57 -0400

    Add initial LDAP connection config

--- config/config.js
+++ config/config.js
@@ -10,7 +10,26 @@ const config = convict({
       env: 'CL_TEMPLATE_DIR'
     }
   },
-  ldap: {}
+  ldap: {
+    hostname: {
+      doc: 'Hostname of the LDAP server.',
+      format: String,
+      default: 'localhost',
+      env: 'CL_LDAP_HOST'
+    },
+    port: {
+      doc: 'Port of the LDAP server.',
+      format: "port",
+      default: 389,
+      env: 'CL_LDAP_PORT'
+    },
+    ssl: {
+      doc: 'Whether to connect to the LDAP server over SSL.',
+      format: Boolean,
+      default: false,
+      env: 'CL_LDAP_SSL'
+    }
+  }
 });
 
 config.validate({ strict: true });
